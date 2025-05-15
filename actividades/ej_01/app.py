c85b2b683938ecbe7b3730947e624cd0de858909
Carlos Gabri Krizak <101573197+cGabri@users.noreply.github.com>
2025-05-15 15:21:23 -0300

    Add files via upload

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,14 @@
-# Aplicacion del servidor
+from microdot import Microdot, send_file
+
+app = Microdot()
+
+@app.get('/')
+async def index(request):
+    return send_file('/index.html')
+
+@app.get('/styles.css')
+async def styles(request):
+    return send_file('/styles.css')
+
+app.run(port=80)
+
